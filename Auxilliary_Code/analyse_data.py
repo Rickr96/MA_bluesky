6fd045e7fd49d0a76b2894f148023a5dccb62157
Rickr96 <rickr@student.ethz.ch>
2023-09-05 15:04:10 +0200

    Run LIFEsim and analyse 'non-det'

--- Auxilliary_Code/analyse_data.py
+++ Auxilliary_Code/analyse_data.py
@@ -2110,7 +2110,7 @@ if __name__ == '__main__':
     # Define the modes you want to run, demo1 and 'all' should be equivalent, 'demo1' is simply for backwards
     # compatibility
     #modes = ['det', 'non-det', 'char']
-    modes = ['det']
+    modes = ['non-det']
     current_dir = Path(__file__).parent.resolve()
     parent_dir = current_dir.parent.resolve()
     results_path = parent_dir.resolve().joinpath("Results/")
